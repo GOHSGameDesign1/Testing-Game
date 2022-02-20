e30b9748356b70dbcea29607cd7d054eebcddaec
GOHSGameDesign1 <GOHSGameDesign1@gmail.com>
2022-02-20 11:38:17 -0800

    Update movement

--- Testing Game/Assets/Scripts/Player.cs
+++ Testing Game/Assets/Scripts/Player.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float speed;
+    public Rigidbody2D rb;
+    private Vector2 directionX;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        directionX = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        rb.MovePosition((Vector2)(transform.position) +  (new Vector2(0,  Time.deltaTime)));
     }
 }
