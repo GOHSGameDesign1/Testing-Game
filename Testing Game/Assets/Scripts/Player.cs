fc687ee667bf402ad59628e629603befbc0fbc37
GOHSGameDesign1 <GOHSGameDesign1@gmail.com>
2022-02-20 11:53:21 -0800

    IT MOVES

--- Testing Game/Assets/Scripts/Player.cs
+++ Testing Game/Assets/Scripts/Player.cs
@@ -11,17 +11,19 @@ public class Player : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
     }
 
     // Update is called once per frame
     void Update()
     {
+        direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         MovePlayer();
+        Debug.Log(Input.GetAxis("Horizontal"));
     }
 
     void MovePlayer()
     {
-        rb.MovePosition((Vector2)transform.position + (direction * speed * Time.deltaTime));
+         rb.MovePosition((Vector2)transform.position + direction * speed * Time.deltaTime);
+        //transform.Translate(new Vector2(Time.deltaTime, 0));
     }
 }
