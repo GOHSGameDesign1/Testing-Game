585f4caac725264d889d932bfa7b3e6a59a0d24a
GOHSGameDesign1 <GOHSGameDesign1@gmail.com>
2022-02-26 14:51:55 -0800

    Updated Clipping Demo

--- Testing Game/Assets/Scripts/PlatformMove.cs
+++ Testing Game/Assets/Scripts/PlatformMove.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class PlatformMove : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public float value;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("je pense que tu pense que nous pensons que vous pensez que");
     }
 
     // Update is called once per frame
     void Update()
     {
         //rb.MovePosition(new Vector2(transform.position.x + 2, transform.position.y));
-        rb.AddForce(Vector2.right * 24440);
+        //rb.AddForce(Vector2.right * 24440);
+        rb.velocity = Vector2.right * 2 * value;
     }
 }
