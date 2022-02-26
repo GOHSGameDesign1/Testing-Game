00a9f9ee7f16b84169fec0fe47bdd43c49d81eeb
GOHSGameDesign1 <GOHSGameDesign1@gmail.com>
2022-02-26 10:22:07 -0800

    Updated Level_1A

--- Testing Game/Assets/Scripts/DEATH.cs
+++ Testing Game/Assets/Scripts/DEATH.cs
@@ -6,16 +6,12 @@ using UnityEngine.SceneManagement;
 public class DEATH : MonoBehaviour
 {
     public int deathCounter;
-    public GameObject baddie;
     public GlobalBool playerDiedDuringLevel;
 
     // Update is called once per frame
     void Update()
     {
-        baddie = GameObject.Find("Baddie");
-        //Debug.Log(SceneManager.GetActiveScene().buildIndex);
 
-        //Debug.Log(playerDiedDuringLevel.value);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
