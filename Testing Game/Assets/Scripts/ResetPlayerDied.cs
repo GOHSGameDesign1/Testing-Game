a0b637ff27aacc351d3e42a30f0069ffcbf3a7c8
GOHSGameDesign2 <GOHSGameDesign2@gmail.com>
2022-02-26 10:39:43 -0800

    yes

---
+++ Testing Game/Assets/Scripts/ResetPlayerDied.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPlayerDied : MonoBehaviour
+{
+
+    public GlobalBool PlayerDiedDuringLevel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerDiedDuringLevel.value = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
